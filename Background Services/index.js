43c0244405ed7e7dd7bb7b4bc0e43b4ae2953c71
Bhavya Sharma <bs707935@gmail.com>
2026-03-14 12:14:22 +0530

    added email services

--- Background Services/index.js
+++ Background Services/index.js
@@ -2,18 +2,22 @@ import express from "express";
 import dotenv from "dotenv";
 import cron from "node-cron";
 import dbConnection from "./utils/db.js";
+import sendWelcomeEmail from "./Email Services/sendWelcomeEmail.js";
+import sendpendingOrderEmail from "./Email Services/sendpendingOrderEmail.js";
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT;
+  dbConnection();
 
 //SCHEDULE SERVICES
 const services = () => {
   cron.schedule("* * * * * *", () => {});
+  sendWelcomeEmail();
+  sendpendingOrderEmail();
 };
 services();
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
-  dbConnection();
 });
---
+++ Background Services/models/user.model.js
@@ -0,0 +1,48 @@
+import mongoose from "mongoose";
+
+const userSchema = mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      require: true,
+    },
+    address: {
+      type: String,
+    },
+    phone: {
+      type: Number,
+    },
+    role: {
+      type: String,
+      default: "user",
+    },
+    status: {
+      type: Number,
+      default: 0,
+    },
+  },
+  {
+    timestamps: true,
+  },
+);
+
+userSchema.pre("save", async function (next){
+  if(!this.isModified("password")) {
+    next();
+  }
+
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+});
+
+const User = mongoose.model("User", userSchema);
+export default User;
